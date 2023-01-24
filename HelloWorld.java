2793254467cb0b468421f16f939d768d6933b131
SohanrajSamadder <sohanrajsamadder4@gmail.com>
2023-01-24 12:57:43 +0530

    Changes Commited

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,9 +1,7 @@
 /* A program to print a string. This is a very first step of our programming journey */
 
-public class HelloWorld 
-{
-    public static void main(String args[])
-    {
+public class HelloWorld {
+    public static void main(String args[]) {
         System.out.println("Hello World");
     }
 }
--- OddOrEven.java
+++ OddOrEven.java
@@ -1,17 +1,15 @@
 import java.util.Scanner;
 
-class OddOrEven
-{
-    public static void main(String args[])
-    {
-        Scanner sc=new Scanner(System.in);
+/*A program to check if a given number is ODD or EVEN*/
+
+class OddOrEven {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
         System.out.println("Enter a number to check if the number is even or odd");
-        int a=sc.nextInt();
-        if(a%2==0)
-        {
+        int a = sc.nextInt();
+        if (a % 2 == 0) {
             System.out.println("The number is even");
-        }
-        else
+        } else
             System.out.println("The number is odd");
     }
 }
